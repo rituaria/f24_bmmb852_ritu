cc1e8bcdea5f4d749902abe3b4628e8d60c70576
Ritu Vadodaria <ritu.vadodaria@gmail.com>
2024-09-25 13:58:02 -0400

    Update ncbi_gff_features.sh

--- hw_04/ncbi_gff_features.sh
+++ hw_04/ncbi_gff_features.sh
@@ -15,27 +15,29 @@ NEWDIR="temp"
 
 # ----------- DO NOT CHANGE BELOW THIS LINE -------------
 
-# Make and go to new directory to download and work into
-mkdir -p ${NEWDIR}_${REFSEQID}
-cd ${NEWDIR}_${REFSEQID}
+# This file was edited with suggestions from class
+
+# Make and go to new directory to download and work into -- unnecessary, just assume that the user is putting the script correctly in whatever directory it is being run
+# mkdir -p ${NEWDIR}_${REFSEQID}
+# cd ${NEWDIR}_${REFSEQID}
 
 # Use NCBI's downloader to obtain GFF file
 datasets download genome accession ${REFSEQID} --include gff3
 
-# Unzip file, name is set by NCBI
-unzip ncbi_dataset.zip
+# Unzip file, name is set by NCBI, -o is for overwriting anything for the rerun without asking
+unzip -o ncbi_dataset.zip
 
 # Enter unzipped directory
-cd ncbi_dataset/data/${REFSEQID}
+#cd ncbi_dataset/data/${REFSEQID}
 
 # Read GFF features column without # lines
-cat genomic.gff |cut -f 3 | grep -v '#' > feature_types.txt
+cat ncbi_dataset/data/${REFSEQID}/genomic.gff |cut -f 3 | grep -v '#' > feature_types.txt
 
 # Read feature_types.txt and output a sorted ranked list of number and type of features
 cat feature_types.txt | sort-uniq-count-rank
 
-# Navigate back to initial working directory
-cd ../../../../
+# Navigate back to initial working directory -- this is dangerous, don't do this, can be easily accidentally changed
+# cd ../../../../
 
-# Remove all temp files
-rm -rf ${NEWDIR}_${REFSEQID}
+# Remove all temp files -- dont do this either
+# rm -rf ${NEWDIR}_${REFSEQID}
